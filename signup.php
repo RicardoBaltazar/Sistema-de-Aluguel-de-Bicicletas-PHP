47d03f8305e51b5fb0eadda4deabcb89c34ac9ab
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-10-15 14:03:43 -0300

    feature - criando pagina de incrição

--- signup.php
+++ signup.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-bt">
-
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,31 +10,38 @@
     <link rel="stylesheet" href="index.css">
     <title>Sitema Bike Rental App</title>
 </head>
-
 <body>
-    <header>
+    
+<header>
         <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#home">Bike Rental App</a>
+                    <a class="navbar-brand" href="home.php">Bike Rental App</a>
                 </div>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Inscrever - se</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="home.php"><span ></span> Home </a></li>
                 </ul>
             </div>
         </nav>
     </header>
 
     <main>
-        <section>
-            <div>
-                <h1>Sitema de Aluguel de bicicletas e Scooters</h1>
-                <p>Você pode escolher vários modelos de bicicletas e scooters e alugar por um mês inteiro.</p>
-            </div>
-            <img src="./assets/undraw_biking_kc4f.svg" alt="">
+        <section class="section-page-signup">
+            <p>Inscrever - se</p>
+
+            <form action="">
+                <label for="name">Nome</label>
+                <input type="text" name="name" placeholder="Digite seu nome" required>
+
+                <label for="email">Email</label>
+                <input type="email" name="email" id="" placeholder="Digite seu email" required>
+
+                <label for="password">Senha</label>
+                <input type="password" name="password" id="" placeholder="Cria uma senha" required>
+
+                <input type="submit" value="Cadastrar" class="input-submit">
+            </form>
         </section>
     </main>
 </body>
-
 </html>
